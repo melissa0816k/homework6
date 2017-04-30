09fb64b731f457628540e842dd304f95dd82f89c
melissa0816k <14203510@life.hkbu.edu.hk>
2017-04-30 12:13:21 +0800

    Create bar.js

---
+++ bar.js
@@ -0,0 +1,51 @@
+option = {
+    title : {
+        text: 'New and Returning Visitors Page View',
+        subtext: 'Google Analytddics'
+    },
+    tooltip : {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['page view']
+    },
+    toolbox: {
+        show : true,
+        feature : {
+            dataView : {show: true, readOnly: false},
+            magicType : {show: true, type: ['line', 'bar']},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    calculable : true,
+    xAxis : [
+        {
+            type : 'category',
+            data : ['HomePage','Homework 1','Homework 2','Homework 3','Homework 4','Homework 5','Others']
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'降水量',
+            type:'bar',
+            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+            markPoint : {
+                data : [
+                    {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183},
+                    {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
+                ]
+            },
+            markLine : {
+                data : [
+                    {type : 'average', name : '平均值'}
+                ]
+            }
+        }
+    ]
+};
